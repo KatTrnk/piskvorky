c73be29b94f70957c1895e39328a997e46c2cbc2
Kateřina Trnková <kat.trnkova@gmail.com>
2023-10-05 19:43:56 +0200

    zjednodušení kódu použitím toggle, 1.bonus

--- hra.js
+++ hra.js
@@ -3,28 +3,16 @@ let currentPlayer = document.querySelector('.Hra__hraje');
 let click = 0;
 
 const selectBtn = (event) => {
-  event.target.classList.add('board__field--circle');
-  currentPlayer.classList.add('Hra__hraje--kolecko');
   click += 1;
   if (click % 2 === 1) {
     event.target.classList.add('board__field--circle');
+    currentPlayer.classList.toggle('Hra__hraje--krizek');
   } else {
     event.target.classList.add('board__field--cross');
-    currentPlayer.classList.remove('Hra__hraje--kolecko');
-    currentPlayer.classList.add('Hra__hraje--krizek');
+    currentPlayer.classList.toggle('Hra__hraje--krizek');
   }
   event.target.disabled = true;
 };
-
-// if (event.target.classList.contains('board__field--circle');) {
-//   symbol+=
-
-// }
-// const cross = document.querySelector('.board__field--circle');
-// cross.classList.toggle('board__field--cross');
-// symbol.textContent += cross;
-
-// currentPlayer.classList.toggle('Hra__hraje--krizek');
 
 document.querySelector('#btn1').addEventListener('click', selectBtn);
 document.querySelector('#btn2').addEventListener('click', selectBtn);
@@ -36,3 +24,12 @@ document.querySelector('#btn7').addEventListener('click', selectBtn);
 document.querySelector('#btn8').addEventListener('click', selectBtn);
 document.querySelector('#btn9').addEventListener('click', selectBtn);
 document.querySelector('#btn10').addEventListener('click', selectBtn);
+
+const confirm = (event) => {
+  const response = window.confirm('Opravdu chceš začít znovu?');
+  if (response === false) {
+    event.preventDefault();
+  }
+};
+
+document.querySelector('#restart').addEventListener('click', confirm);
